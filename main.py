659358af4993bfd70cf9e25c400c1ec53c0a4086
Ilia Javakhishvili <ilo@Ilias-iMac.local>
2024-07-17 23:12:59 +0200

    Countdown app - Project using modules, loops etc.

---
+++ main.py
@@ -0,0 +1,9 @@
+from helper import validate_and_execute
+
+user_input = " "
+while user_input != "exit":
+    user_input = input("enter a number and and conversion unit:\n")
+    days_and_units = user_input.split(":")
+    days_and_units_dictionary = {"days": days_and_units[0], "unit": days_and_units[1]}
+    print(days_and_units_dictionary)
+    validate_and_execute(days_and_units_dictionary)
